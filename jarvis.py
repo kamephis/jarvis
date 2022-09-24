63dccb09f65f2c0c1aa3687d1861ad049a8af17d
Marlon Boehland <boehland@outlook.com>
2022-09-24 21:09:18 +0200

    added hue

--- jarvis.py
+++ jarvis.py
@@ -1,22 +1,108 @@
 import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+from hueber.api import Bridge
+from hueber.lib import LightBuilder
 
 listener = sr.Recognizer()
-command = ''
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+bridgeUser = "RA9X7Ngd5z9EVGEvZoVNa-j7D8r58zAYfxXiQ0-N"
 
-with sr.Microphone() as source:
-    print("listening...")
-    listener.adjust_for_ambient_noise(source=source)
-    voice = listener.listen(source, timeout=3)
+# 4 = female computer voice (natural), 7 = jarvis
+engine.setProperty('voice', voices[7].id)
 
-try:
-    command = listener.recognize_google(voice)
-    command = command.lower()
+hue = Bridge("192.168.178.20", bridgeUser)
 
-    if 'jarvis' in command:
-        print("You said: " + listener.recognize_google(voice))
 
-except sr.UnknownValueError:
-    print(" Error")
+def turn_on_office():
+    print(hue.groups)
+    new_update = LightBuilder()
+    new_update["on"] = True
+    new_update["bri"] = 254
+    hue.groups[4].push(new_update.update_str())
 
-except sr.RequestError as e:
-    print("Request Error")
+
+def turn_off_office():
+    print(hue.groups)
+    new_update = LightBuilder()
+    new_update["on"] = False
+    new_update["bri"] = 254
+    hue.groups[4].push(new_update.update_str())
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("listening...")
+
+            listener.adjust_for_ambient_noise(source=source)
+            voice = listener.listen(source, timeout=3)
+
+            command = listener.recognize_google(voice)
+            command = command.lower()
+
+            if 'jarvis' in command:
+                command = command.replace('jarvis', '')
+                print(command)
+
+            talk(command)
+
+    except sr.UnknownValueError:
+        pass
+        # print(" Error")
+
+    except sr.RequestError as e:
+        print("Request Error")
+        pass
+    return command
+
+
+def run_jarvis():
+    command = take_command()
+    print(command)
+    if 'office on' in command:
+        turn_on_office()
+        talk('Turning on office lights')
+
+    if 'office off' in command:
+        turn_off_office()
+        talk('Turning off office lights')
+
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+
+    elif 'who is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+
+    else:
+        talk('Please say the command again.')
+
+while True:
+    run_jarvis()
